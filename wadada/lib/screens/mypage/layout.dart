a4a9afb539e8f7a18a9ef8fd7a65c336c3943d65
benyy0101 <benyy0101@gmail.com>
2024-04-24 10:27:41 +0900

    REFACTORING: smallNav seperated out

--- wadada/lib/screens/mypage/layout.dart
+++ wadada/lib/screens/mypage/layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wadada/common/const/colors.dart';
+import 'package:wadada/screens/mypage/smallNav.dart';
 
 class MyPageLayout extends StatelessWidget {
   const MyPageLayout({Key? key}) : super(key: key);
@@ -16,58 +17,6 @@ class MyPageLayout extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [SmallNav(), RecordList()],
       ),
-    );
-  }
-}
-
-class SmallNav extends StatelessWidget {
-  const SmallNav({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        IconButton(
-            onPressed: () {},
-            tooltip: '내 기록을 봅니다',
-            icon: const Column(
-              children: [
-                Icon(
-                  Icons.history_edu_rounded,
-                  size: 50.0,
-                ),
-                SizedBox(height: 5.0),
-                Text('내 기록')
-              ],
-            )),
-        IconButton(
-            onPressed: () {},
-            tooltip: '',
-            icon: const Column(
-              children: [
-                Icon(
-                  Icons.sentiment_very_satisfied_rounded,
-                  size: 50,
-                ),
-                SizedBox(height: 5.0),
-                Text('아바타')
-              ],
-            )),
-        IconButton(
-            onPressed: () {},
-            tooltip: '프로필을 수정합니다.',
-            icon: const Column(
-              children: [
-                Icon(
-                  Icons.access_alarm,
-                  size: 50.0,
-                ),
-                SizedBox(height: 5.0),
-                Text('프로필 수정')
-              ],
-            )),
-      ],
     );
   }
 }
