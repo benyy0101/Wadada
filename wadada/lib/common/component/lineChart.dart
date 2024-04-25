d76157e14bc94c757dc1b66680d8c98aaf056be6
benyy0101 <benyy0101@gmail.com>
2024-04-25 11:22:06 +0900

    S10P31A704-117 FEAT: line chart generalized

--- wadada/lib/common/component/lineChart.dart
+++ wadada/lib/common/component/lineChart.dart
@@ -2,37 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 import 'package:wadada/common/const/colors.dart';
+import 'package:intl/intl.dart';
 
-class SalesData {
-  SalesData(this.year, this.sales);
-  final DateTime year;
-  final double sales;
+class chartData {
+  chartData(this.distance, this.numerics);
+  final int distance;
+  final double numerics;
 }
 
-class LineChart extends StatelessWidget {
-  final List<SalesData> chartData = [
-    SalesData(DateTime(2010), 35),
-    SalesData(DateTime(2011), 28),
-    SalesData(DateTime(2012), 34),
-    SalesData(DateTime(2013), 32),
-    SalesData(DateTime(2014), 40)
-  ];
+class LineChart<T extends chartData> extends StatelessWidget {
+  final List<T> chartData;
+  final String metrics;
+  const LineChart({
+    super.key,
+    required this.chartData,
+    required this.metrics,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: 300,
-      child: SfCartesianChart(
-          backgroundColor: OATMEAL_COLOR,
-          primaryXAxis: DateTimeAxis(),
-          series: <CartesianSeries>[
-            // Renders line chart
-            LineSeries<SalesData, DateTime>(
-                dataSource: chartData,
-                xValueMapper: (SalesData sales, _) => sales.year,
-                yValueMapper: (SalesData sales, _) => sales.sales)
-          ]),
+    return Center(
+      child: Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width * 0.9,
+        padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: OATMEAL_COLOR,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.7),
+              spreadRadius: 0,
+              blurRadius: 5.0,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: SfCartesianChart(
+            primaryXAxis: NumericAxis(
+              majorGridLines: MajorGridLines(color: Colors.transparent),
+              numberFormat: NumberFormat('### km'),
+            ),
+            primaryYAxis: NumericAxis(
+                numberFormat: NumberFormat('### $metrics'),
+                rangePadding: ChartRangePadding.additional,
+                //Aligns the y-axis labels
+                anchorRangeToVisiblePoints: false),
+            backgroundColor: OATMEAL_COLOR,
+            palette: [
+              DARK_GREEN_COLOR
+            ],
+            series: <CartesianSeries>[
+              // Renders line chart
+              LineSeries<T, int>(
+                  dataSource: chartData,
+                  xValueMapper: (data, _) => data.distance,
+                  yValueMapper: (data, _) => data.numerics)
+            ]),
+      ),
     );
   }
 }
--- wadada/lib/common/component/myRecords.dart
+++ wadada/lib/common/component/myRecords.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:wadada/common/component/lineChart.dart';
 
+class temp extends chartData {
+  temp(super.distance, super.numerics);
+}
+
 class MyRecords extends StatelessWidget {
-  const MyRecords({super.key});
+  final List<temp> chartData = [
+    temp(12, 160),
+    temp(13, 150),
+    temp(14, 120),
+    temp(15, 80),
+    temp(16, 110)
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [LineChart()],
-      ),
+      body: Center(
+          child: LineChart(
+        chartData: chartData,
+        metrics: 'hz',
+      )),
     );
   }
 }
