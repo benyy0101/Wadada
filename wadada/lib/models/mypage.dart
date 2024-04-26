932f769a46cbea38336597470e03e513a395e15e
benyy0101 <benyy0101@gmail.com>
2024-04-26 15:31:14 +0900

    S10P31A704-120 FEAT: serializable & repository

--- wadada/lib/models/mypage.dart
+++ wadada/lib/models/mypage.dart
@@ -1,18 +1,65 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:wadada/util/serializable.dart';
 
-class RecordRequest {
+//Request는 직렬화(Serializable)를, Response는 역직렬화 해줘야 사용가능함
+TimeOfDay _parseTimeOfDay(String timeString) {
+  final hoursAndMinutes = timeString.split(':');
+  return TimeOfDay(
+    hour: int.parse(hoursAndMinutes[0]),
+    minute: int.parse(hoursAndMinutes[1]),
+  );
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  Point(this.x, this.y);
+
+  // JSON을 Dart 객체로 변환하는 팩토리 생성자
+  factory Point.fromJson(Map<String, dynamic> json) {
+    return Point(
+      json['x'] as double,
+      json['y'] as double,
+    );
+  }
+
+  // Dart 객체를 JSON으로 변환하는 메소드
+  Map<String, dynamic> toJson() => {
+        'x': x,
+        'y': y,
+      };
+}
+
+class RecordRequest implements Serializable {
   final int recordSeq;
   final String recordType;
 
   RecordRequest({required this.recordSeq, required this.recordType});
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'recordSeq': recordSeq,
+      'recordType': recordType,
+    };
+  }
 }
 
 //MYPAGE-001
 class MonthlyRecord {
   final List<SimpleRecord> monthlyRecord;
   MonthlyRecord({required this.monthlyRecord});
+
+  factory MonthlyRecord.fromJson(Map<String, dynamic> json) {
+    var list = json['monthlyRecord'] as List;
+    List<SimpleRecord> monthlyRecordList =
+        list.map((item) => SimpleRecord.fromJson(item)).toList();
+
+    return MonthlyRecord(monthlyRecord: monthlyRecordList);
+  }
 }
 
 class SimpleRecord {
@@ -27,6 +74,15 @@ class SimpleRecord {
       required this.recordType,
       required this.recordDist,
       required this.recordSeq});
+
+  factory SimpleRecord.fromJson(Map<String, dynamic> json) {
+    return SimpleRecord(
+      recordSeq: json['recordSeq'] as int,
+      recordType: json['recordType'] as String,
+      recordDist: json['recordDist'] as double,
+      recordCreatedAt: DateTime.parse(json['recordCreatedAt'] as String),
+    );
+  }
 }
 
 //MYPAGE-002
@@ -62,6 +118,33 @@ class SingleDetail {
     required this.recordMeanPace,
     required this.recordMeanHeartbeat,
   });
+
+  factory SingleDetail.fromJson(Map<String, dynamic> json) {
+    var speedList =
+        (json['recordSpeed'] as List).map((i) => Point.fromJson(i)).toList();
+    var heartbeatList = (json['recordHeartbeat'] as List)
+        .map((i) => Point.fromJson(i))
+        .toList();
+    var paceList =
+        (json['recordPace'] as List).map((i) => Point.fromJson(i)).toList();
+
+    return SingleDetail(
+      recordType: json['recordType'],
+      recordRank: json['recordRank'],
+      recordImage: json['recordImage'],
+      recordDist: json['recordDist'],
+      recordTime: _parseTimeOfDay(json['recordTime']),
+      recordStartLocation: Point.fromJson(json['recordStartLocation']),
+      recordEndLocation: Point.fromJson(json['recordEndLocation']),
+      recordSpeed: speedList,
+      recordHeartbeat: heartbeatList,
+      recordPace: paceList,
+      recordCreatedAt: DateTime.parse(json['recordCreatedAt']),
+      recordMeanSpeed: json['recordMeanSpeed'],
+      recordMeanPace: json['recordMeanPace'],
+      recordMeanHeartbeat: json['recordMeanHeartbeat'],
+    );
+  }
 }
 
 //MYPAGE-003
@@ -83,6 +166,33 @@ class MultiDetail extends SingleDetail {
       required super.recordMeanSpeed,
       required super.recordMeanPace,
       required super.recordMeanHeartbeat});
+
+  factory MultiDetail.fromJson(Map<String, dynamic> json) {
+    List<Rank> rankings = (json['rankings'] as List)
+        .map((rankJson) => Rank.fromJson(rankJson))
+        .toList();
+
+    return MultiDetail(
+      rankings: rankings,
+      recordType: json['recordType'],
+      recordRank: json['recordRank'],
+      recordImage: json['recordImage'],
+      recordDist: json['recordDist'].toDouble(),
+      recordTime: _parseTimeOfDay(json['recordTime']),
+      recordStartLocation: Point.fromJson(json['recordStartLocation']),
+      recordEndLocation: Point.fromJson(json['recordEndLocation']),
+      recordSpeed:
+          List<Point>.from(json['recordSpeed'].map((x) => Point.fromJson(x))),
+      recordHeartbeat: List<Point>.from(
+          json['recordHeartbeat'].map((x) => Point.fromJson(x))),
+      recordPace:
+          List<Point>.from(json['recordPace'].map((x) => Point.fromJson(x))),
+      recordCreatedAt: DateTime.parse(json['recordCreatedAt']),
+      recordMeanSpeed: json['recordMeanSpeed'].toDouble(),
+      recordMeanPace: json['recordMeanPace'].toDouble(),
+      recordMeanHeartbeat: json['recordMeanHeartbeat'].toDouble(),
+    );
+  }
 }
 
 class Rank {
@@ -91,34 +201,82 @@ class Rank {
   final String profileImage;
 
   Rank({required this.name, required this.metric, required this.profileImage});
+  factory Rank.fromJson(Map<String, dynamic> json) {
+    return Rank(
+      name: json['name'],
+      metric: json['metric'],
+      profileImage: json['profileImage'],
+    );
+  }
 }
 
 //MYPAGE-004
 class MarathonDetail extends SingleDetail {
   final List<RankMarathon> rankings;
-  MarathonDetail(
-      {required this.rankings,
-      required super.recordType,
-      required super.recordRank,
-      required super.recordImage,
-      required super.recordDist,
-      required super.recordTime,
-      required super.recordStartLocation,
-      required super.recordEndLocation,
-      required super.recordSpeed,
-      required super.recordHeartbeat,
-      required super.recordPace,
-      required super.recordCreatedAt,
-      required super.recordMeanSpeed,
-      required super.recordMeanPace,
-      required super.recordMeanHeartbeat});
+
+  MarathonDetail({
+    required this.rankings,
+    required super.recordType,
+    required super.recordRank,
+    required super.recordImage,
+    required super.recordDist,
+    required super.recordTime,
+    required super.recordStartLocation,
+    required super.recordEndLocation,
+    required super.recordSpeed,
+    required super.recordHeartbeat,
+    required super.recordPace,
+    required super.recordCreatedAt,
+    required super.recordMeanSpeed,
+    required super.recordMeanPace,
+    required super.recordMeanHeartbeat,
+  });
+
+  factory MarathonDetail.fromJson(Map<String, dynamic> json) {
+    var rankingsList = (json['rankings'] as List)
+        .map((item) => RankMarathon.fromJson(item))
+        .toList();
+
+    return MarathonDetail(
+      rankings: rankingsList,
+      recordType: json['recordType'],
+      recordRank: json['recordRank'],
+      recordImage: json['recordImage'],
+      recordDist: json['recordDist'].toDouble(),
+      recordTime: _parseTimeOfDay(json['recordTime']),
+      recordStartLocation: json['recordStartLocation'],
+      recordEndLocation: json['recordEndLocation'],
+      recordSpeed: json['recordSpeed'].toDouble(),
+      recordHeartbeat: json['recordHeartbeat'].toDouble(),
+      recordPace: json['recordPace'].toDouble(),
+      recordCreatedAt: DateTime.parse(json['recordCreatedAt']),
+      recordMeanSpeed: json['recordMeanSpeed'].toDouble(),
+      recordMeanPace: json['recordMeanPace'].toDouble(),
+      recordMeanHeartbeat: json['recordMeanHeartbeat'].toDouble(),
+    );
+  }
 }
 
 class RankMarathon extends Rank {
   final int rank;
-  RankMarathon(
-      {required this.rank,
-      required super.name,
-      required super.metric,
-      required super.profileImage});
+
+  RankMarathon({
+    required this.rank,
+    required String name,
+    required String metric,
+    required String profileImage,
+  }) : super(
+          name: name,
+          metric: metric,
+          profileImage: profileImage,
+        );
+
+  factory RankMarathon.fromJson(Map<String, dynamic> json) {
+    return RankMarathon(
+      rank: json['rank'],
+      name: json['name'],
+      metric: json['metric'],
+      profileImage: json['profileImage'],
+    );
+  }
 }
---
+++ wadada/lib/repository/mypageRepo.dart
@@ -0,0 +1,63 @@
+import 'package:get/get.dart';
+import 'package:wadada/models/mypage.dart';
+import 'package:wadada/provider/mypageProvider.dart';
+
+//어떤 api가 있는지 정리
+abstract class AbstractMypageRepository {
+  Future<MonthlyRecord> getMonthlyRecord(DateTime date);
+  Future<SingleDetail> getSingleDetail(RecordRequest req);
+  Future<MultiDetail> getMultiDetail(RecordRequest req);
+  Future<MarathonDetail> getMarathonDetail(RecordRequest req);
+}
+
+class MypageRepository extends GetxService implements AbstractMypageRepository {
+  final MypageAPI mypageAPI;
+
+  MypageRepository({
+    required this.mypageAPI,
+  });
+
+  @override
+  Future<MarathonDetail> getMarathonDetail(RecordRequest req) async {
+    try {
+      Response res = await mypageAPI.getMarathonDetail(req);
+      return MarathonDetail.fromJson(res.body);
+    } catch (e) {
+      print("ERROR: $e");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MonthlyRecord> getMonthlyRecord(DateTime date) async {
+    try {
+      Response res = await mypageAPI.getMonthlyRecord(date);
+      return MonthlyRecord.fromJson(res.body);
+    } catch (e) {
+      print("ERROR: $e");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MultiDetail> getMultiDetail(RecordRequest req) async {
+    try {
+      Response res = await mypageAPI.getMultiDetail(req);
+      return MultiDetail.fromJson(res.body);
+    } catch (e) {
+      print("ERROR: $e");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<SingleDetail> getSingleDetail(RecordRequest req) async {
+    try {
+      Response res = await mypageAPI.getMultiDetail(req);
+      return SingleDetail.fromJson(res.body);
+    } catch (e) {
+      print("ERROR: $e");
+      rethrow;
+    }
+  }
+}
