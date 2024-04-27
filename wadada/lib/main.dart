53a4d567801e8f9367f8ef5d07e7ed2c1327fa32
sueminpark <breathin_suemin@naver.com>
2024-04-27 23:45:06 +0900

    S10P31A704-109 FEAT: kakao login except state management

--- wadada/lib/main.dart
+++ wadada/lib/main.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:wadada/common/component/myRecords.dart';
-//import 'package:wadada/common/pages/mainpage.dart';
+import 'package:wadada/screens/mainpage/layout.dart';
 import 'package:wadada/screens/mypage/layout.dart';
 import 'package:wadada/screens/mainpage/layout.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+      nativeAppKey: '9430c9bd5cf24477bf3ccb0cde068f16',
+      javaScriptAppKey: '671973e2cb29da502bdead673817f346',
+  );
+
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: MyRecords(),
+      home: MainPageLayout(),
     );
   }
 }
--- wadada/lib/screens/mainpage/layout.dart
+++ wadada/lib/screens/mainpage/layout.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:wadada/common/const/colors.dart';
 
 class MainPageLayout extends StatelessWidget {
-  const MainPageLayout({Key? key}) : super(key: key);
+  const MainPageLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +56,7 @@ class MainPageLayout extends StatelessWidget {
           const SizedBox(
             height: 50
           ),
-          _KakaoButton(),
+          KakaoLoginButton(),
         ],
       ),
     );
@@ -61,24 +64,81 @@ class MainPageLayout extends StatelessWidget {
 }
 
 
+class KakaoLoginButton extends StatelessWidget {
+  const KakaoLoginButton({super.key});
 
-class _KakaoButton extends StatelessWidget {
-  const _KakaoButton({Key? key}) : super(key: key);
-  
+  // 카카오톡 앱을 통한 로그인 시도
+  void _loginWithKakaoTalk(BuildContext context) async {
+    try {
+      var token = await UserApi.instance.loginWithKakaoTalk();
+      print('카카오톡으로 로그인 성공, 액세스 토큰: ${token.accessToken}');
+    } catch (error) {
+      print('카카오톡으로 로그인 실패: $error');
+      _loginWithKakaoAccount(context); // 카카오톡 로그인 실패 시, 카카오 계정 로그인 시도
+    }
+  }
+
+  // 카카오 계정(이메일/비밀번호)을 통한 로그인 시도
+  void _loginWithKakaoAccount(BuildContext context) async {
+    try {
+      var token = await UserApi.instance.loginWithKakaoAccount();
+      print('카카오 계정으로 로그인 성공, 액세스 토큰: ${token.accessToken}');
+    } catch (error) {
+      print('카카오 계정으로 로그인 실패: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(1, 5),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () => signWithKakao(),
+      child: Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(1, 5),
+            ),
+          ],
+        ),
+        child: Image.asset('assets/images/kakao_button.png'),
       ),
-      child: Image.asset('assets/images/kakao_button.png'),
     );
+  }
+}
+
+
+signWithKakao() async {
+  // 카카오톡 실행 가능 여부 확인
+  // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+  if (await isKakaoTalkInstalled()) {
+    try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+
+      // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+      // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+      if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+      }
+      // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+      try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+      } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  } else {
+    try {
+      await UserApi.instance.loginWithKakaoAccount();
+      print('카카오계정으로 로그인 성공');
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
+    }
   }
 }
