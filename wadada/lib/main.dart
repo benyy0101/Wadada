fd325eb2633250506313527daba5bc2e4ab2fd7f
jjeong41 <yj_w41l@naver.com>
2024-04-30 14:12:15 +0900

    S10P31A704-132 FEAT: single run page

--- wadada/lib/main.dart
+++ wadada/lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
-// import 'package:wadada/common/pages/mainpage.dart';
-import 'package:wadada/screens/mypage/layout.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+// import 'package:wadada/common/component/myRecords.dart';
 import 'package:wadada/screens/mainpage/layout.dart';
-import 'package:wadada/screens/singlemainpage/single_main.dart';
+//import 'package:wadada/common/pages/mainpage.dart';
+// import 'package:wadada/screens/mypage/layout.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:wadada/screens/singlemainpage/single_main.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: 'assets/env/.env');
+  
+  String nativeAppKey = dotenv.env['NATIVE_APP_KEY'] ?? "기본값"; 
+  String javaScriptAppKey = dotenv.env['JAVASCRIPT_APP_KEY'] ?? "기본값";
+
   final appKey = dotenv.env['APP_KEY'] ?? '';
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+      nativeAppKey: nativeAppKey,
+      javaScriptAppKey: javaScriptAppKey,
+  );
+
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
