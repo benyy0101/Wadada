c27caa826028b5ac96741859fe5d957d68bb9138
유현종 <guswhd903@naver.com>
2024-05-03 13:59:44 +0900

    S10P31A704-102 HOTFIX : AOP https

--- Multi/src/main/java/org/api/wadada/aspect/AccessTokenAspect.java
+++ Multi/src/main/java/org/api/wadada/aspect/AccessTokenAspect.java
@@ -18,17 +18,17 @@
             // 원래의 메서드를 실행합니다.
             Object proceed = joinPoint.proceed();
 
-            // 현재 HTTP 요청과 응답 객체를 가져옵니다.
-            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-            HttpServletRequest request = attributes.getRequest();
+            // 현재 HTTP 요청 객체를 가져옵니다.
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String protocol = request.getScheme(); // "http" 또는 "https"
 
-            if (request != null) {
+            if("https".equalsIgnoreCase(protocol) || "http".equalsIgnoreCase(protocol)) {
                 boolean isAccessTokenUpdated = checkAccessTokenUpdate(request);
 
                 if (isAccessTokenUpdated && proceed instanceof ResponseEntity) {
                     // 업데이트된 액세스 토큰을 헤더에 추가하고 새로운 ResponseEntity를 반환합니다.
                     ResponseEntity<?> originalResponse = (ResponseEntity<?>) proceed;
-                    return ResponseEntity.status(HttpServletResponse.SC_CREATED)
+                    return ResponseEntity.status(originalResponse.getStatusCode())
                             .headers(originalResponse.getHeaders())
                             .header("X-Access-Token-Updated", "true")
                             .header("AccessToken-Updated", request.getAttribute("newAccessToken").toString())
@@ -39,6 +39,7 @@
             // 액세스 토큰이 업데이트되지 않았거나, 반환 타입이 ResponseEntity가 아닌 경우 원래의 반환 값을 그대로 반환합니다.
             return proceed;
         }
+
 
         private boolean checkAccessTokenUpdate(HttpServletRequest request) {
             // 요청 객체에서 newAccessToken 속성 값을 가져옵니다.
