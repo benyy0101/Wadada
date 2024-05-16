a7267597c0a081e24dfcb082f5468ce471affad0
유현종 <guswhd903@naver.com>
2024-05-16 15:33:56 +0900

    S10P31A704-221 fix timezone

--- Marathon/src/main/java/org/api/wadada/WadadaApplication.java
+++ Marathon/src/main/java/org/api/wadada/WadadaApplication.java
@@ -13,7 +13,7 @@ public class WadadaApplication {
 	@PostConstruct
 	public void started() {
 		// timezone UTC 셋팅
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 	public static void main(String[] args) {
