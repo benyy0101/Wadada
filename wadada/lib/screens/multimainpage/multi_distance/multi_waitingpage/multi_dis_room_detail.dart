3b429ce112a212d0f8c17f8082bfbd029b250c12
benyy0101 <benyy0101@gmail.com>
2024-05-16 10:27:34 +0900

    S10P31A704-230 FIX: isOwner observable

--- wadada/lib/screens/multimainpage/multi_distance/multi_waitingpage/multi_dis_room_detail.dart
+++ wadada/lib/screens/multimainpage/multi_distance/multi_waitingpage/multi_dis_room_detail.dart
@@ -383,59 +383,54 @@ class _MultiRoomDetailState extends State<MultiRoomDetail> {
               SizedBox(height: 30),
 
               //팀원일때 준비 버튼
-              Visibility(
-                visible: controller.isOwner,
-                child: TextButton(
-                  onPressed: () {
-                    controller.ready(roomInfo.roomIdx);
-                  },
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor:
-                        isButtonPressed ? Colors.grey[400] : GREEN_COLOR,
-                    padding: EdgeInsets.only(
-                        left: 155, right: 155, top: 10, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+              Obx(() {
+                if (!controller.isOwner.value) {
+                  return TextButton(
+                    onPressed: () {
+                      controller.ready(roomInfo.roomIdx);
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor:
+                          isButtonPressed ? Colors.grey[400] : GREEN_COLOR,
+                      padding: EdgeInsets.only(
+                          left: 155, right: 155, top: 10, bottom: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
                     ),
-                  ),
-                  child: Text(
-                    '준비완료',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              //방장일때 레디 버튼
-              Visibility(
-                  visible: !controller.isOwner,
-                  child: Obx(() {
-                    print("controller.numREady--------------");
-                    print(controller.numReady);
-                    return TextButton(
-                      onPressed:
+                    child: Text(
+                      '준비완료',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  );
+                } else {
+                  return TextButton(
+                    onPressed:
+                        controller.numReady == controller.members.length - 1
+                            ? () {
+                                controller.gameStart();
+                              }
+                            : null,
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor:
                           controller.numReady == controller.members.length - 1
-                              ? () {
-                                  controller.gameStart();
-                                }
-                              : null,
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor:
-                            controller.numReady == controller.members.length - 1
-                                ? GREEN_COLOR
-                                : Colors.grey[400],
-                        padding: EdgeInsets.only(
-                            left: 155, right: 155, top: 10, bottom: 10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
+                              ? GREEN_COLOR
+                              : Colors.grey[400],
+                      padding: EdgeInsets.only(
+                          left: 155, right: 155, top: 10, bottom: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                      child: Text(
-                        '시작하기',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    );
-                  })),
+                    ),
+                    child: Text(
+                      '시작하기',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  );
+                }
+              }),
               SizedBox(height: 10),
               TextButton(
                 onPressed: () {
