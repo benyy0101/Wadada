459191cc19718b7bb68e152591b92e624046e2da
sueminpark <breathin_suemin@naver.com>
2024-05-17 14:34:22 +0900

    FEAT: heartrate task

--- wdd_watch/lib/screens/components/heartbeat.dart
+++ wdd_watch/lib/screens/components/heartbeat.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:wadada/commons/const/colors.dart';
 import 'dart:async';
 
+import 'package:watch_connectivity/watch_connectivity.dart';
+
 class runHeart extends StatefulWidget {
   const runHeart({super.key});
 
@@ -11,23 +13,39 @@ class runHeart extends StatefulWidget {
 }
 
 class _runHeartState extends State<runHeart> {
+
+  // 코틀린에서 심박센서에서 가져온 값
   static const MethodChannel _channel = MethodChannel('com.ssafy.wadada/heart_rate');
   // 초기 심박수 일단 설정을 ??로 해두고
   String _heartRate = '??';
   // 컨트롤러 슛
   final StreamController<String> _streamController = StreamController<String>();
 
+  // 워치앱에서 플러터앱으로 보낼 심박수 값 
+  final WatchConnectivityBase _watch = WatchConnectivity();
+  final MethodChannel _watchChannel = MethodChannel('watch_connectivity');
+  var _supported = false;
+  var _paired = false;
+  var _reachable = false;
+  bool _connected = false;
+  final _log = <String>[];
+
   @override
   void initState() {
     super.initState();
 
-    // 추가 코드
+    // 워치센서에서 심박수 받는 것
     _channel.setMethodCallHandler(_handleMethod);
     _streamController.stream.listen((heartRate) {
       setState(() {
         _heartRate = heartRate;
       });
     });
+
+    // 워치에서 앱으로 심박수 보내는
+    // 워치 연결 상태 확인 및 세션 활성화
+    initPlatformState();
+    _initWear();
   }
 
   // 안드로이드 네이티브 코드에서 호출될 메서드 핸들러 (추가)
@@ -41,6 +59,41 @@ class _runHeartState extends State<runHeart> {
         break;
     }
   }
+
+
+  // 워치 관련코드
+  void _initWear() {
+    _watch.messageStream.listen((message) => setState(() {
+      _connected = true;
+    }));
+  }
+
+  void sendMessage(_heartRate) {
+    final message = {
+      '_heartRate': _heartRate,
+    };
+    _watch.sendMessage(message);
+    setState(() => _log.add('메세지: $message'));
+
+  }
+
+  void sendContext(_heartRate) {
+    final context = {
+      '_heartRate': _heartRate,
+    };
+    _watch.updateApplicationContext(context);
+    setState(() => _log.add('보내진 context: $context'));
+  }
+
+  void initPlatformState() async {
+    _supported = await _watch.isSupported;
+    _paired = await _watch.isPaired;
+    _reachable = await _watch.isReachable;
+    setState(() {
+
+    });
+  }
+
 
   // 추가 코드
   @override
